196dae14bb60fd3a5224905be0c6d82550f56e5a
@garethosullivan <garethosullivan@gmail.com>
2025-06-27 21:38:49 +0100

    Add full user auth, protected routes, and dashboard

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+// pages/_app.js
+import { SessionProvider } from 'next-auth/react';
+
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
